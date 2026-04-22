384424bd8d054607e027f9b7cb5f6bcbbf58fcca
Mel <melvindjones400@gmail.com>
2026-04-22 00:10:33 -0500

    Created About section with filler text and pic

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import About from "@/components/About";
 
 // Use the NextPage type for the Home component
 const Home: NextPage = () => {
@@ -19,12 +20,14 @@ const Home: NextPage = () => {
       <Header />
 
       {/* Hero Section */}
-      <section id="hero" className="snap-center">
+      <section id="hero" className="snap-start">
         <Hero />
       </section>
 
       {/* About */}
-
+      <section id="about" className="snap-center">
+        <About />
+      </section>
       {/* Experience */}
 
       {/* Skills */}
