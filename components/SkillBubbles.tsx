c3d9d2cce3f1342729f8297503d9ca8a99082adf
Mel <121836871+melvinjones2@users.noreply.github.com>
2026-04-23 13:58:22 -0500

    Created project section

--- components/SkillBubbles.tsx
+++ components/SkillBubbles.tsx
@@ -16,12 +16,12 @@ function SkillBubbles({ directionLeft }: Props) {
         transition={{ duration: 1 }}
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
-        className="rounded-full border border-gray-500 object-cover w-20 h-20 md:w-24 md:h-24 xl:w-27 xl:h-27 filter group-hover:grayscale transition duration-300 ease-in-out"
+        className="rounded-full border border-gray-500 object-cover w-20 h-20 md:w-20 md:h-20 xl:w-27 xl:h-27 filter group-hover:grayscale transition duration-300 ease-in-out"
         src="https://cdn.sanity.io/images/xbn4zmfs/production/ff00c08760983e0e037aaf6ab4e004f4d147276a-383x383.png"
         // {/* CHANGE THIS!!!!!!!!*/}
         alt="Skill Icon"
       />
-      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-20 h-20 md:w-24 md:h-24 xl:w-27 xl:h-27 rounded-full z-0">
+      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-20 w-20 md:w-20 md:h-20 xl:w-27 xl:h-27 rounded-full z-0">
         <div className="flex items-center justify-center h-full">
           <p className="text-3xl font-bold text-black opacity-100">100%</p>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,15 +5,15 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import WorkExperience from "@/components/WorkExperience";
 import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
 
 // Use the NextPage type for the Home component
 const Home: NextPage = () => {
   return (
     <div
       className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory 
-    overflow-scroll z-0"
-    >
-      {/* scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80  */}
+    overflow-scroll z-0">
+
       <Head>
         <title>Mel's Portfolio</title>
       </Head>
@@ -43,6 +43,9 @@ const Home: NextPage = () => {
       </section>
 
       {/* Projects */}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
 
       {/* Contact Me */}
     </div>
