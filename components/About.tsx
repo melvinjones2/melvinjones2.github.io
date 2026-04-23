62c630cd56a6b1ed1064260397ddd6918c9e8295
Mel <melvindjones400@gmail.com>
2026-04-22 23:28:49 -0500

    Fixed About section image clipping

--- components/About.tsx
+++ components/About.tsx
@@ -13,7 +13,7 @@ export default function About({}: Props) {
         transition={{ duration: 1.5, }}
         className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-16 uppercase tracking-[20px] text-gray-500 text-2xl">
         About
       </h3>
 
@@ -28,10 +28,10 @@ export default function About({}: Props) {
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
         src={aboutPic.src}
-        className="mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
+        className="-mb-24 md:mb-0 flex-shrink-0 w-52 h-52 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
       />
 
-      <div className="space-y-10 px-0 md:px-10 font-semibold">
+      <div className="space-y-5 px-0 md:px-10 font-semibold">
         <h4 className="text-4xl font-semibold">
             Here is a {""} 
             <span className="underline decoration-[#F7AB0A]/50">little</span>{" "}
